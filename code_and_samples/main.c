7a1160f4105208abbcb6dac42e6b523a3f90503f
Asger Allin Jensen <143587255+AsgerAJ@users.noreply.github.com>
2024-09-11 15:34:52 +0200

    Erosion extension

--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -1,8 +1,8 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_inv.bmp
+// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
+// To run (win): main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,39 +11,45 @@
 /**** Global Variables ****/
 unsigned const threshold = 90;
 
-//Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+// Function to invert pixels of an image (negative)
+void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
+        output_image[x][y][c] = 255 - input_image[x][y][c];
       }
     }
   }
 }
 
-void greyScale2d(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGTH]){
+void greyScale2d(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGTH])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
-      greyscale_image[x][y] = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2])/3;
+      greyscale_image[x][y] = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
     }
   }
 }
 
-void bitThreshold(unsigned char input_gs_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_bit_image[BMP_WIDTH][BMP_HEIGTH]){
-  for (int x = 0; x < BMP_WIDTH; x++){
-    for (int y = 0; y < BMP_HEIGTH; y++){
+void bitThreshold(unsigned char input_gs_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_bit_image[BMP_WIDTH][BMP_HEIGTH])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
       output_bit_image[x][y] = (input_gs_image[x][y] <= threshold) ? 0 : 255;
     }
   }
 }
 
-void applyChannels(unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+void applyChannels(unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
@@ -56,42 +62,87 @@ void applyChannels(unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGTH], unsigne
   }
 }
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char greyscale_image [BMP_WIDTH][BMP_HEIGTH];
-  unsigned char output_bit_image [BMP_WIDTH][BMP_HEIGTH];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-
-//Main function
-int main(int argc, char** argv)
+char erode(unsigned char bit_image[BMP_WIDTH][BMP_HEIGTH])
 {
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
+  unsigned char eroded_image[BMP_WIDTH][BMP_HEIGTH];
+  unsigned char waseroded = 0;
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      if (bit_image[x][y])
+      {
+        if (!(bit_image[x - 1][y] && bit_image[x + 1][y] && bit_image[x][y - 1] && bit_image[y + 1]))
+        {
+          eroded_image[x][y] = 0;
+          waseroded = 1;
+        }
+        else
+        {
+          eroded_image[x][y] = 255;
+        }
+      }
+    }
+  }
+  // Replaces 
+  for (int replacex = 0; replacex < BMP_WIDTH; replacex++){
+    for (int replacey = 0; replacey < BMP_HEIGTH; replacey++){
+      bit_image[replacex][replacey] = eroded_image[replacex][replacey]; 
+    }
+  }
+  return waseroded;
+}
 
-  //Checking that 2 arguments are passed
+void detect(){
+
+}
+
+// Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char greyscale_image[BMP_WIDTH][BMP_HEIGTH];
+unsigned char output_bit_image[BMP_WIDTH][BMP_HEIGTH];
+unsigned char eroded_img[BMP_WIDTH][BMP_HEIGTH];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+
+// Main function
+int main(int argc, char **argv)
+{
+  // argc counts how may arguments are passed
+  // argv[0] is a string with the name of the program
+  // argv[1] is the first command line argument (input image)
+  // argv[2] is the second command line argument (output image)
+
+  // Checking that 2 arguments are passed
   if (argc != 3)
   {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+    exit(1);
   }
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file
   read_bitmap(argv[1], input_image);
 
-  //Run greyscale
-  greyScale2d(input_image,greyscale_image);
+  // Run greyscale
+  greyScale2d(input_image, greyscale_image);
 
-  //Threshold
-  bitThreshold(greyscale_image,output_bit_image);
+  // Threshold
+  bitThreshold(greyscale_image, output_bit_image);
 
-  //Turn to 3d array
-  applyChannels(output_bit_image,output_image);
+  while(1)
+  {
+    if(erode(output_bit_image)){
+      break;
+    }else{
+      printf("Image was eroded");
+    }
+  }
 
-  //Save image to file
+  // Turn to 3d array
+  applyChannels(output_bit_image, output_image);
+
+  // Save image to file
   write_bitmap(output_image, argv[2]);
 
   printf("Done!\n");
