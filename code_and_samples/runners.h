8be1a8de27152e3996e9ce2c5874885cd2a48593
Asger Allin Jensen <143587255+AsgerAJ@users.noreply.github.com>
2024-10-02 14:51:32 +0200

    Revert "used pointers to swap arrays"
    This reverts commit 884bbe0968a3bb4b2ad34e734c8c67908ac531b9.

--- code_and_samples/runners.h
+++ code_and_samples/runners.h
@@ -25,22 +25,23 @@ int runImproved(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
     unsigned int maxTravel = round(detectionSize / 2);
     unsigned char imageToProcess1[BMP_WIDTH][BMP_HEIGTH];
     unsigned char imageToProcess2[BMP_WIDTH][BMP_HEIGTH];
-    unsigned char(*ptr1)[BMP_WIDTH] = imageToProcess1; // pointer for swapping
-    unsigned char(*ptr2)[BMP_WIDTH] = imageToProcess2; // pointer for swapping
+    int iteration = 0;
 
     // Run greyscale
     greyScale2d(input_image, imageToProcess1);
 
-    // betterCustomThreshold(imageToProcess,applyOtsu(imageToProcess));
+    //betterCustomThreshold(imageToProcess,applyOtsu(imageToProcess));
 
     dynamicOtsu(imageToProcess1);
 
-    while (erodeImprovement(ptr1, ptr2))
+    while (erodeImprovement(imageToProcess1,imageToProcess2,iteration))
     {
-        detectImprovement(ptr1, input_image);
-        unsigned char(*temp)[BMP_WIDTH] = ptr2;
-        ptr2 = ptr1;
-        ptr1 = temp;
+        if(iteration % 2 == 0){
+            detectImprovement(imageToProcess2, input_image);
+        }else{
+            detectImprovement(imageToProcess1, input_image);
+        }
+        iteration++;
     }
     return totalCount;
 }
