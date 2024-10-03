d0a8019743d2dccf8dffd3f9560afa6608e2a466
NiklasLysdal <s235235@dtu.dk>
2024-10-03 18:21:16 +0200

    Added dynamic otsu optimization attempt as comment

--- code_and_samples/dynamicOtsu.h
+++ code_and_samples/dynamicOtsu.h
@@ -87,3 +87,105 @@ void dynamicOtsu(unsigned char inputImage[BMP_WIDTH][BMP_HEIGTH]) {
         }
     }
 }
+
+/* 
+Attempt to optimize dynamic otsu. Didnt work, at least for the small sizes were working with.
+
+unsigned char otsuSmall(unsigned char img[BMP_WIDTH][BMP_HEIGTH], int width, int height, int startX, int startY)
+{
+    int histogram[256] = {0};
+    float cumulative[256] = {0.0f};
+    float var[256] = {0.0f};
+    int totalPixels = width * height;
+
+    float meanTotal = 0.0f;
+
+    for (int x = startX; x < startX + width; x++)
+    {
+        for (int y = startY; y < startY + height; y++)
+        {
+
+            histogram[img[x][y]]++;
+        }
+    }
+
+
+
+    int cumulativeSum = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        cumulativeSum += histogram[i];
+        cumulative[i] = cumulativeSum / (float)totalPixels;
+    }
+
+    float maxVariance = 0;
+    unsigned char bestThreshold = 0;
+
+    for (int i = 0; i < 256; i++)
+    {
+        int sumClass1 = 0;
+        int sumClass2 = 0;
+        float meanClass1 = 0.0f;
+        float meanClass2 = 0.0f;
+
+        for (int j = 0; j <= i; j++)
+        {
+            sumClass1 += j * histogram[j];
+        }
+        if (cumulative[i] > 0)
+        {
+            meanClass1 = sumClass1 / (cumulative[i] * totalPixels);
+        }
+
+        for (int j = i + 1; j < 256; j++)
+        {
+            sumClass2 += j * histogram[j];
+        }
+        if (1 - cumulative[i] > 0)
+        {
+            meanClass2 = sumClass2 / ((1 - cumulative[i]) * totalPixels);
+        }
+
+        var[i] = cumulative[i] * (1 - cumulative[i]) * (meanClass1 - meanClass2) * (meanClass1 - meanClass2);
+
+        if (var[i] > maxVariance)
+        {
+            maxVariance = var[i];
+            bestThreshold = i;
+        }
+    }
+    for (int x = startX; x < startX + width; x++)
+    {
+        for (int y = startY; y < startY + height; y++)
+        {
+            img[x][y] = (img[x][y] > threshold) ? 255 : 0;
+        }
+    }
+    return bestThreshold;
+}
+
+void dynamicOtsu(unsigned char inputImage[BMP_WIDTH][BMP_HEIGTH])
+{
+    unsigned int maxDistanceVert = BMP_HEIGTH / SPLITS;
+    unsigned int maxDistanceHorz = BMP_WIDTH / SPLITS;
+
+    for (int row = 0; row < SPLITS; row++)
+    {
+        for (int column = 0; column < SPLITS; column++)
+        {
+            otsuSmall(inputImage, maxDistanceHorz, maxDistanceVert, row * maxDistanceHorz, column * maxDistanceVert);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+*/
